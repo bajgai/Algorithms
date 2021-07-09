03e4cb198c9db872962d551fff43c5dd9e085cc9
Niranjan <bajgainiranjan@gmail.com>
2021-07-09 17:19:39 -0600

    complete exercise 1.1.2

--- src/exercise111.java
+++ src/exercise111.java
@@ -1,5 +1,10 @@
 public class exercise111 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int a = (0 + 15) / 2;
+        System.out.println(String.format("a = %d", a));
+        double b = 2.0 * Math.exp(-6) * 100000000.1;
+        System.out.println(String.format("b = %g", b));
+        boolean c = true && false || true && true;
+        System.out.println(String.format("c = %b", c));
     }
 }
